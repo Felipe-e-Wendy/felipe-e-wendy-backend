b077bfba13dd766a9c72ce4d6add5a2e82746d2b
Felipe Markson <fmarkson@gmail.com>
2020-12-27 17:41:10 -0400

    Add: DataBase

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,28 @@
-from typing import Optional
-
 from fastapi import FastAPI, Response, status
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 import os
 import requests
+
+from .models import FromForm
+from .database import db
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="*",
+    allow_origin_regex=".*localhost.*",
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "https://felipe-e-wendy.github.io/",
+    ],
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
+
+
+@app.on_event("shutdown")
+async def disconnect():
+    db.close()
 
 
 @app.get("/")
@@ -21,24 +30,26 @@ async def read_root():
     return {"status": "OK"}
 
 
-class Item(BaseModel):
-    name: str
-    wpp: str
-    price: float
-    msg: str
-    token: str
-
-
-@app.post("/data/", status_code=200)
-async def create_item(item: Item, response: Response):
+@app.post("/msg", status_code=200)
+async def create_item(msg: FromForm, response: Response):
     SECRET_RECAPTCHAV2 = os.environ["SECRET_RECAPTCHAV2"]
-    data = {"secret": SECRET_RECAPTCHAV2, "response": item.token}
+    data = {"secret": SECRET_RECAPTCHAV2, "response": msg.token}
     response_captcha = requests.post(
         "https://www.google.com/recaptcha/api/siteverify", data=data
     ).json()
 
     if response_captcha["success"]:
-        return item
+        document = dict()
+        msg_dict = msg.dict()
+        document["name"] = msg_dict["name"]
+        document["wpp"] = msg_dict["wpp"]
+        document["msg"] = msg_dict["msg"]
+        document["price"] = msg_dict["price"]
+        document["payment_id"] = "test paid id"
+        document["paid"] = False
+        db.msg.insert_one(document.copy())
+        return document
+
     else:
         response.status_code = status.HTTP_401_UNAUTHORIZED
-        return item
+        return {"error": "Captcha Error"}
